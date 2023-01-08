0215691c8e0257cfe9509904570c9f953bfbbb91
Kristina Adigamova <kriwowak@yandex.ru>
2023-01-08 21:46:56 +0500

    Исправлено Задание 3

--- src/Zadanie3.java
+++ src/Zadanie3.java
@@ -2,15 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Zadanie3 {
-    private final Map<String, Integer> test = new HashMap<>();
-    public void main(String[] args) {
+    private static final Map<String, Integer> test = new HashMap<>();
+    public static void main(String[] args) {
         test.put("Test", 11);
         test.put("Testik", 34);
+        test.put("Test", 34);
     }
 
-    public void addNumber(String s, Integer i){
-        if (test.containsKey(s) && test.containsValue(i)){
-            throw new RuntimeException(test + " ключ уже есть и значения совпадают");
+    public static void addNumber(String s, Integer i){
+        if (test.keySet().equals(i)){
+            throw new RuntimeException(test + " ключ уже есть или значения совпадают");
         }
         test.put(s,i);
     }
